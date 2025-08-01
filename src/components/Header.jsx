0cb01725eaf596ed3a31263295995f41165c54f3
dinukasw <dinukasanthush50@gmail.com>
2025-08-02 00:30:08 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src="" alt="" />
+            <h1></h1>
+        </div>
+        <nav>
+            <button>
+                Cart (0)
+            </button>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
