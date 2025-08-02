0fec6f00c25ecb7762101710acccc6f0da40f6e1
dinukasw <dinukasanthush50@gmail.com>
2025-08-02 06:11:53 +0530

    create cart context and and include cart context to add to cart button and headerr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import { useContext } from 'react'
 import logoImg from '../assets/logo.jpg'
 import Button from './UI/button'
+import CartContext from '../store/CartContext'
 
 const Header = () => {
+  const cartCtx = useContext(CartContext);
+
+  const totalItems = cartCtx.items.reduce((total, item) => {
+      return total + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
         <div id="title">
@@ -10,7 +18,7 @@ const Header = () => {
         </div>
         <nav>
             <Button textOnly>
-                Cart (0)
+                Cart ({totalItems})
             </Button>
         </nav>
     </header>
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/formatting"
 import Button from "./UI/button"
+import CartContext from "../store/CartContext";
 
 const MealItem = ({meal}) => {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
   return (
     <li className="meal-item">
         <article>
@@ -12,7 +19,7 @@ const MealItem = ({meal}) => {
                 <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
-                <Button>Add to Cart</Button>
+                <Button onClick={handleAddMealToCart}>Add to Cart</Button>
             </p>
         </article>
     </li>
