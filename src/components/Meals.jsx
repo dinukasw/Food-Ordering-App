11d5701398d3b08968ff36fdccbdc3e218a8759f
dinukasw <dinukasanthush50@gmail.com>
2025-08-13 03:35:44 +0530

    create loading and error handling in meal fetching

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,23 @@
-
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 
 const requestConfig = {};
 
 const Meals = () => {
-    const {data:loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, [])
+    const {
+        data: loadedMeals,
+        isLoading,
+        error,
+    } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+    if (isLoading) {
+        return <p className="center">Fetching Data.....</p>;
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />;
+    }
 
     return (
         <ul id="meals">
